85ee882a74c7c1aafae043565584e770298e1e42
arthur262 <46006708+arthur262@users.noreply.github.com>
2022-02-25 00:31:45 -0400

    计时器还没做完

--- 自适应菜单栏/main.js
+++ 自适应菜单栏/main.js
@@ -7,13 +7,16 @@ window.onload = function(){
 window.onresize=function(){
   var nav= document.getElementById("main_nav");
   var sec_nav=document.getElementById("sec_menu");
+  var image=document.getElementById("logo");
   if(document.body.clientWidth<820){
-    nav.style.width="50%";
-    nav.style.opacity="0";
+    window.setInterval(change(nav),10);
+
     sec_nav.style.display="block";
+    image.style.transform="translate(5ch)";
   }else{
     sec_nav.style.display="none";
     nav.style.opacity="1";
+    image.style.transform="translate(0ch)";
   }
 }
 
@@ -23,21 +26,20 @@ window.onresize=function(){
 function show_iteam(x, id) {
   var triger = document.getElementById(id);
   if (x == 1) {
-    sleep(500).then(() => {
+    
       triger.className = "show_iteam show_iteam_transition";
-      triger.style.display = "block";
-    });
+      // triger.style.display = "block";
   } else {
     if (triger.mouseover) {
       console.log("yes");
     } else {
-      sleep(500).then(() => {
+      
         triger.className = "show_iteam";
-        triger.style.display = "";
-      });
+        // triger.style.display = "";
+    };
     }
   }
-}
+
 
 // menu切换用途
 var count = 0;
@@ -60,4 +62,11 @@ function show() {
     document.getElementById("bottom").className = "  bottom-b bottom-a-s";
     count = 0;
   }
+}
+// https://zhidao.baidu.com/question/331862687918168765.html
+var i=0;
+function change(o){
+  i++;
+  o.style.opacity = (100-i)/100; 
+  if(i>=100) i=100;
 }
