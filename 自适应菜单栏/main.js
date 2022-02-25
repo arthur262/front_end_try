33d16ce1d4bfd74261df65b11387b8883ca665ca
arthur262 <46006708+arthur262@users.noreply.github.com>
2022-02-25 15:17:42 -0400

    完成自适应侧边栏

--- 自适应菜单栏/main.js
+++ 自适应菜单栏/main.js
@@ -1,51 +1,52 @@
+window.onload = function () {
+  test();
+};
+window.onresize = function () {
+  test();
+};
+
 function sleep(time) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
 
-window.onload = function(){
-}
-window.onresize=function(){
-  var nav= document.getElementById("main_nav");
-  var sec_nav=document.getElementById("sec_menu");
-  var image=document.getElementById("logo");
-  if(document.body.clientWidth<820){
-    window.setInterval(change(nav),10);
+/* 用于处理menu的逻辑的*/
+function test() {
+  // sec_nav=document.getElementsByClassName("login_nav")[0].childNodes;
+  var mobile = document.getElementById("container_s");
+  var web = document.getElementById("container");
 
-    sec_nav.style.display="block";
-    image.style.transform="translate(5ch)";
-  }else{
-    sec_nav.style.display="none";
-    nav.style.opacity="1";
-    image.style.transform="translate(0ch)";
+  if (document.body.clientWidth < 820) {
+    web.style.display = "none";
+    mobile.style.display = "grid";
+  } else {
+    web.style.display = "grid";
+    mobile.style.display = "none";
   }
 }
-
-
 
 /*用于显示悬停 */
 function show_iteam(x, id) {
   var triger = document.getElementById(id);
   if (x == 1) {
-    
-      triger.className = "show_iteam show_iteam_transition";
-      // triger.style.display = "block";
+    triger.className = "show_iteam show_iteam_transition";
+    // triger.style.display = "block";
   } else {
     if (triger.mouseover) {
       console.log("yes");
     } else {
-      
-        triger.className = "show_iteam";
-        // triger.style.display = "";
-    };
+      triger.className = "show_iteam";
+      // triger.style.display = "";
     }
   }
-
+}
 
 // menu切换用途
 var count = 0;
 function show() {
+  var side = document.getElementById("side");
   count = count + 1;
   if (count % 2 == 1) {
+    // open
     document.getElementById("A-contanier").className =
       " A-contanier A-contanier-a";
     document.getElementById("top").className = " top top-a ";
@@ -55,18 +56,18 @@ function show() {
       document.getElementById("top").className = "top-a-s ";
       document.getElementById("bottom").className = "bottom-a-s ";
     }, "1000");
+    side.style.display = "block";
   } else {
+    //close
     document.getElementById("A-contanier").className =
       "  A-contanier-b A-contanier";
     document.getElementById("top").className = "  top-b top-a-s";
     document.getElementById("bottom").className = "  bottom-b bottom-a-s";
     count = 0;
+    side.className = "side side-out";
+
+    setTimeout(function () {
+      side.style.display = "none";
+    }, 1500);
   }
-}
-// https://zhidao.baidu.com/question/331862687918168765.html
-var i=0;
-function change(o){
-  i++;
-  o.style.opacity = (100-i)/100; 
-  if(i>=100) i=100;
 }
